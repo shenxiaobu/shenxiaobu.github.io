ed5aa4e578d53601d5144eaed0b6231e760272a3
shenxiaobu <18850285681@163.com>
2023-05-06 13:03:12 +0800

    feat: 新增vue3文档

---
+++ vue3-element-dict/data/dict.js
@@ -0,0 +1,52 @@
+export default {
+  SEX: [
+    {
+      value: "1",
+      label: "男"
+    },
+    {
+      value: "2",
+      label: "女"
+    },
+    {
+      value: "3",
+      label: "未知"
+    }
+  ],
+  niceOrBad: [
+    {
+      "value": "0",
+      "label": "好"
+    }, {
+      "value": "1",
+      "label": "差"
+    }
+  ],
+  area: [
+    {
+      "id": "110000",
+      "parentId": "0",
+      "label": "北京"
+    },
+    {
+      "id": "110100",
+      "parentId": "0",
+      "label": "北京市"
+    },
+    {
+      "id": "110101",
+      "parentId": "110100",
+      "areaName": "东城区"
+    },
+    {
+      "id": "110102",
+      "parentId": "110100",
+      "areaName": "西城区"
+    },
+    {
+      "id": "110105",
+      "parentId": "110100",
+      "areaName": "朝阳区"
+    }
+  ]
+}
