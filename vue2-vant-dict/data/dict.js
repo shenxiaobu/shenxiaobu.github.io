02ba5e6bb61fa593852c0752d8de1e099024d1f9
shenyanxiang <shenyanxiang@ylzinfo.com>
2023-06-29 12:42:09 +0800

    提交代码

---
+++ vue2-vant-dict/data/dict.js
@@ -0,0 +1,96 @@
+
+export default {
+  gender: [
+    {
+      "value": "000",
+      "label": "注销"
+    }, {
+      "value": "001",
+      "label": "正常"
+    }
+  ],
+  "country": [
+    {
+      "id": "35062400",
+      "parentId": "0",
+      "areaName": "诏安县"
+    },
+    {
+      "id": "35062401",
+      "parentId": "35062400",
+      "areaName": "太平镇"
+    },
+    {
+      "id": "35062402",
+      "parentId": "35062400",
+      "areaName": "金星乡"
+    },
+    {
+      "id": "35062403",
+      "parentId": "35062400",
+      "areaName": "南诏镇"
+    },
+    {
+      "id": "35062404",
+      "parentId": "35062400",
+      "areaName": "霞葛镇"
+    }
+  ],
+  "city": [
+    {
+      "id": "35060000",
+      "parentId": "0",
+      "areaName": "漳州市"
+    },
+    {
+      "id": "35062400",
+      "parentId": "35060000",
+      "areaName": "诏安县"
+    },
+    {
+      "id": "35062401",
+      "parentId": "35062400",
+      "areaName": "太平镇"
+    },
+    {
+      "id": "35062402",
+      "parentId": "35062400",
+      "areaName": "金星乡"
+    },
+    {
+      "id": "35062403",
+      "parentId": "35062400",
+      "areaName": "南诏镇"
+    },
+    {
+      "id": "35062404",
+      "parentId": "35062400",
+      "areaName": "霞葛镇"
+    },
+    {
+      "id": "35062300",
+      "parentId": "35060000",
+      "areaName": "云霄县"
+    },
+    {
+      "id": "35062301",
+      "parentId": "35062300",
+      "areaName": "列屿镇"
+    },
+    {
+      "id": "35062302",
+      "parentId": "35062300",
+      "areaName": "陈岱镇"
+    },
+    {
+      "id": "35062303",
+      "parentId": "35062300",
+      "areaName": "云陵镇"
+    },
+    {
+      "id": "35062304",
+      "parentId": "35062300",
+      "areaName": "火田镇"
+    }
+  ]
+}
